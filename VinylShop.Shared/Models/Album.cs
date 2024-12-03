8905497cbf83eabc95bd23a7eb736789249f769d
Khasanov.MI <Khasanov.MI@D3072-203-SMV>
2024-12-03 13:44:31 +0300

    created

---
+++ VinylShop.Shared/Models/Album.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VinylShop.Shared.Models
+{
+    public class Album
+    {
+        public int AlbumId { get; set; }
+        public string Name { get; set; } = "";
+        public string Description { get; set; } = "";
+        public string Image { get; set; } = "";
+        public string AuthorName { get; set; } = "";
+        public int TimeInMinutes { get; set; }
+        public string TimeFormatted => $"{TimeInMinutes / 60}h {TimeInMinutes % 60}m";
+        public int Raiting { get; set; }
+        public IEnumerable<Track> Tracks { get; set; } = Array.Empty<Track>();
+    }
+}
