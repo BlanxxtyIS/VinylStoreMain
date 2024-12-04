a60305f0b65937baf4c3caa20daa03b095c2981d
Khasanov.MI <Khasanov.MI@D3072-203-SMV>
2024-12-04 11:32:45 +0300

    svd

---
+++ VinylShop.Api/Controllers/AlbumIteration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VinylShop.Api.Service;
+
+namespace VinylShop.Api.Controllers
+{
+    public class AlbumIteration : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
